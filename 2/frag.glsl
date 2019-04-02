00f919099d0ecef73e272de26e895fa39eb4377b
melnyczuk <h.melnyczuk@gmail.com>
2019-04-02 07:48:00 +0100

    Add animation functionality

--- 2/frag.glsl
+++ 2/frag.glsl
@@ -6,10 +6,14 @@ uniform vec2 u_res;
 void main() {
   vec2 st = gl_FragCoord.xy/u_res;
 
-  float pct = 0.0;
+  float r = distance(st,vec2((cos(u_time + 0.64) + 1.0) / 2.0, (tan(u_time + 0.44) + 1.0) / 2.0));
+  float g = distance(st,vec2((sin(u_time + 0.34) + 1.0) / 2.0, (cos(u_time + 0.64) + 1.0) / 2.0));
+  float b = distance(st,vec2((tan(u_time + 0.44) + 1.0) / 2.0, (sin(u_time + 0.34) + 1.0) / 2.0));
 
-  pct = distance(st,vec2(0.5));
-  vec3 color = vec3(pct);
+  vec4 rect = vec4(0.2, 0.2, 0.6, 0.6);
+  vec2 hv = step(rect.xy, st) * step(st, rect.zw);
+  float onOff = hv.x * hv.y;
 
-  gl_FragColor = vec4( color, 1.0 );
+  gl_FragColor = mix(vec4(r, g, b, 1.0 ), vec4(1.0,1.0,1.0,1.0), onOff);
 }
+
