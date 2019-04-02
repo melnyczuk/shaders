b9ba4a9c13c0899b62ca4052befd04b157f89e11
melnyczuk <howard.melnyczuk@bbc.co.uk>
2019-04-02 14:59:35 +0100

    do shader better

--- 2/frag.glsl
+++ 2/frag.glsl
@@ -6,16 +6,24 @@ uniform vec2 u_res;
 void main() {
   vec2 st = gl_FragCoord.xy/u_res;
 
-  float r = distance(st,vec2(clamp(abs(cos(u_time + 0.64)), 0.1, 0.7), clamp(abs(atan(u_time + 0.44)), 0.1, 0.7)));
+  float r = distance(st,vec2(clamp(abs(cos(u_time + 0.64)), 0.1, 0.7), clamp(abs(cos(u_time + 0.44)), 0.1, 0.7)));
   float g = distance(st,vec2(clamp(abs(sin(u_time + 0.34)), 0.1, 0.7), clamp(abs(cos(u_time + 0.64)), 0.1, 0.7)));
-  float b = distance(st,vec2(clamp(abs(tan(u_time + 0.44)), 0.1, 0.9), clamp(abs(sin(u_time + 0.34)), 0.1, 0.9)));
+  float b = distance(st,vec2(clamp(abs(sin(u_time + 0.44)), 0.1, 0.9), clamp(abs(sin(u_time + 0.34)), 0.1, 0.9)));
 
-  vec4 rect = vec4(0.325, 0.5, 0.75, 1.0);
-  vec2 hv = step(rect.xy, st) * step(st, rect.zw);
+  float center = 0.39;
+  vec4 rect0 = vec4(center, 0.05, 0.683, 1.0);
+  vec2 hv = step(rect0.xy, st) * step(st, rect0.zw);
   float onOff = hv.x * hv.y;
+
+  vec4 rect1 = vec4(0.1, 0.05,center, 1.0);
+  vec2 hv1 = step(rect1.xy, st) * step(st, rect1.zw);
+  float onOff1 = hv1.x * hv1.y;
 
   float a = (cos(u_time + 0.64) + 1.2) / 1.4;
 
-  gl_FragColor = mix(vec4(r,g,b, 1.0), vec4(g,r,b,a), onOff);
+  vec4 mixa = mix(vec4(0.5*r,g,b,a),vec4(r,0.2*g,b, 1.0), onOff);
+  vec4 mixb = mix(vec4(r,0.2*g,b, 1.0), vec4(r,g,0.5*b,a), onOff1);
+
+  gl_FragColor = mix(mixa, mixb, 0.5);
 }
 
