7b486c94c7ef6b1acd5fcdb51aad4b54e6450bbe
melnyczuk <h.melnyczuk@gmail.com>
2019-04-02 08:55:36 +0100

    Get square in right place

--- 2/frag.glsl
+++ 2/frag.glsl
@@ -6,14 +6,16 @@ uniform vec2 u_res;
 void main() {
   vec2 st = gl_FragCoord.xy/u_res;
 
-  float r = distance(st,vec2((cos(u_time + 0.64) + 1.0) / 2.0, (tan(u_time + 0.44) + 1.0) / 2.0));
-  float g = distance(st,vec2((sin(u_time + 0.34) + 1.0) / 2.0, (cos(u_time + 0.64) + 1.0) / 2.0));
-  float b = distance(st,vec2((tan(u_time + 0.44) + 1.0) / 2.0, (sin(u_time + 0.34) + 1.0) / 2.0));
+  float r = distance(st,vec2(clamp(abs(cos(u_time + 0.64)), 0.1, 0.7), clamp(abs(atan(u_time + 0.44)), 0.1, 0.7)));
+  float g = distance(st,vec2(clamp(abs(sin(u_time + 0.34)), 0.1, 0.7), clamp(abs(cos(u_time + 0.64)), 0.1, 0.7)));
+  float b = distance(st,vec2(clamp(abs(tan(u_time + 0.44)), 0.1, 0.9), clamp(abs(sin(u_time + 0.34)), 0.1, 0.9)));
 
-  vec4 rect = vec4(0.2, 0.2, 0.6, 0.6);
+  vec4 rect = vec4(0.325, 0.5, 0.75, 1.0);
   vec2 hv = step(rect.xy, st) * step(st, rect.zw);
   float onOff = hv.x * hv.y;
 
-  gl_FragColor = mix(vec4(r, g, b, 1.0 ), vec4(1.0,1.0,1.0,1.0), onOff);
+  float a = (cos(u_time + 0.64) + 1.2) / 1.4;
+
+  gl_FragColor = mix(vec4(r,g,b, 1.0), vec4(g,r,b,a), onOff);
 }
 
