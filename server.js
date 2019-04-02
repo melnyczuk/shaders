b03f231ec45e4e142568b38a81245b331d108692
Howard Melnyczuk <h.melnyczuk@gmail.com>
2019-04-02 06:49:01 +0100

    Day 1 (#1)

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static(__dirname + '/'));
+
 app.get('/:day', (req, res) => res.sendFile(`${__dirname}/${req.params.day}/index.html`));
-app.get('/:day/index.js', (req, res) => res.sendFile(`${__dirname}/${req.params.day}/index.js`));
-app.get('/:day/vert', (req, res) => res.sendFile(`${__dirname}/${req.params.day}/vert.glsl`));
-app.get('/:day/frag', (req, res) => res.sendFile(`${__dirname}/${req.params.day}/frag.glsl`));
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
